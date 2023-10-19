ce5f95fbaa41a7023e803b441590857663f1e84e
Anuraag Muktevi <anuraagmuktevi@Anuraags-MBP.dhost.uta.edu>
2023-10-18 22:14:50 -0500

    Redux added to manage state and use auth tokens

--- src/App.js
+++ src/App.js
@@ -8,24 +8,28 @@ import Instructor from './pages/Instructor/Instructor';
 import ProgramCoordinator from './pages/ProgramCoordinator/ProgramCoordinator';
 import ChatBot from './templates/CommonTemplates/ChatBot';
 import Admin from './pages/Admin/Admin';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 function AppRouter() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path='/'>
-            <Route index element={<Home></Home>}></Route>
-            <Route path="student" element={<Student></Student>}></Route>
-            <Route path="qa" element={<QualityAssurance></QualityAssurance>}></Route>
-            <Route path="programcoordinator" element={<ProgramCoordinator></ProgramCoordinator>}></Route>
-            <Route path="instructor" element={<Instructor></Instructor>}></Route>
-            <Route path="admin" element={<Admin></Admin>}></Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-      <ChatBot></ChatBot>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path='/'>
+              <Route index element={<Home></Home>}></Route>
+              <Route path="student" element={<Student></Student>}></Route>
+              <Route path="qa" element={<QualityAssurance></QualityAssurance>}></Route>
+              <Route path="programcoordinator" element={<ProgramCoordinator></ProgramCoordinator>}></Route>
+              <Route path="instructor" element={<Instructor></Instructor>}></Route>
+              <Route path="admin" element={<Admin></Admin>}></Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+        <ChatBot></ChatBot>
+      </div>
+    </Provider>
   );
 }
 
