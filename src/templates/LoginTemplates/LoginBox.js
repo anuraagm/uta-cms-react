8d9e25c9589993ca8d8e306d00e5f20d411c0767
Anuraag Muktevi <anuraagmuktevi@Anuraags-MBP.dhost.uta.edu>
2023-10-27 19:03:17 -0500

    Rectified forms of login and signup

--- src/templates/LoginTemplates/LoginBox.js
+++ src/templates/LoginTemplates/LoginBox.js
@@ -13,14 +13,15 @@ function LoginBox({toggleLoginPopup, toggleSignupPopup}) {
 
   const handleLogin = () => {
     // Add your login logic here
-    console.log('Logging in with:', email, password);
+    if (email !== "" && password !== "") {
+      console.log('Logging in with:', email, password);
+    }
   };
 
   const handleSignUp = () => {
     // Add your sign-up logic here
     toggleSignupPopup();
     toggleLoginPopup();
-    console.log('Signing up with:', email, password);
   };
 
   const handleForgotPassword = () => {
@@ -30,35 +31,39 @@ function LoginBox({toggleLoginPopup, toggleSignupPopup}) {
 
   return isLoginBoxVisible ? (
     <div className="login-box">
-      <button className="close-button" onClick={handleCloseButton}>X</button>
-      <h1 className='bg-blue text-white p-4 text-lg'>UTA Login</h1>
-      <br />
-      <h2>Please login/signup to continue</h2>
-      <br />
-      <input
-        type="text"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        id = 'email_'
-      />
-      <br />
-      <br />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        id = 'password_'
-      />
-      <br />
-      <br />
-      <button onClick={handleForgotPassword} id='forgot_password'>Forgot Password / username ?</button>
-      <br />
-      <button onClick={handleLogin} id='login_button'>Login</button>
-      <br />
-      <br />
-      <button onClick={handleSignUp} id='signup_button'>Sign Up</button>
+      <form action=''>
+        <button className="close-button" onClick={handleCloseButton}>X</button>
+        <h1 className='bg-blue text-white p-4 text-lg'>UTA Login</h1>
+        <br />
+        <h2>Please login/signup to continue</h2>
+        <br />
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className = 'email_'
+          required
+        />
+        <br />
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className = 'password_'
+          required
+        />
+        <br />
+        <br />
+        <button onClick={handleForgotPassword} className='forgot_password'>Forgot Password / username ?</button>
+        <br />
+        <button onClick={handleLogin} className='login_button'>Login</button>
+        <br />
+        <br />
+        <button onClick={handleSignUp} className='signup_button'>Sign Up</button>
+      </form>
     </div>
   ) : null;
 }
--- src/templates/LoginTemplates/SignupBox.js
+++ src/templates/LoginTemplates/SignupBox.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./LoginBox.css";
+import axios from 'axios';
+
 
 function SignupBox({toggleSignupPopup, toggleLoginPopup}) {
   const [firstname, setFirstName] = useState("");
@@ -12,7 +14,7 @@ function SignupBox({toggleSignupPopup, toggleLoginPopup}) {
   const [isSignupBoxVisible, setIsSignupBoxVisible] = useState(true);
 
   useEffect(() => {
-    if (password === confirmPassword && password !== "") {
+    if (password === confirmPassword && password !== "" && confirmPassword !== "") {
       setConfirmMessage(true);
     }
     else {
@@ -33,8 +35,8 @@ function SignupBox({toggleSignupPopup, toggleLoginPopup}) {
 
   const handleSignup = () => {
     // Add your Signup logic here
-    if (confirmMessage) {
-      console.log("Logging in with:", email, password);
+    if (confirmMessage && firstname !== "" && lastname !== "" && phone !== "" && email !== "") {
+      console.log("Signup initiated");
     } 
   };
 
@@ -46,76 +48,83 @@ function SignupBox({toggleSignupPopup, toggleLoginPopup}) {
 
   return isSignupBoxVisible ? (
     <div className="Signup-box">
-      <button className="close-button" onClick={handleCloseButton}>X</button>
-      <h1 className='bg-blue text-white p-4 text-lg'>UTA Signup</h1>
-      <br />
-      <h2>Please signup to continue</h2>
-      <br />
-      <input
-        type="text"
-        placeholder="First Name"
-        value={firstname}
-        onChange={(e) => setFirstName(e.target.value)}
-        id="email_"
-      />
-      <br />
-      <br />
-      <input
-        type="text"
-        placeholder="Last Name"
-        value={lastname}
-        onChange={(e) => setLastName(e.target.value)}
-        id="email_"
-      />
-      <br />
-      <br />
-      <input
-        type="text"
-        placeholder="phone"
-        value={phone}
-        onChange={(e) => setPhone(e.target.value)}
-        id="email_"
-      />
-      <br />
-      <br />
-      <input
-        type="text"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        id="email_"
-      />
-      <br />
-      <br />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        id="password_"
-      />
-      <br />
-      <br />
-      <input
-        type="password"
-        placeholder="confirm Password"
-        value={confirmPassword}
-        onChange={(e) => {setConfirmPassword(e.target.value);}}
-        id="password_"
-      />
-      {
-        !confirmMessage && <h1>Both passwords should match</h1>
-      }
-      <br />
-      <br />
-      <button onClick={handleSignup} id="login_button">
-        Signup
-      </button>
-      <br />
-      <br />
-      <button onClick={handleCancelButton} id="signup_button">
-        Cancel
-      </button>
+      <form action="">
+        <button className="close-button" onClick={handleCloseButton}>X</button>
+        <h1 className='bg-blue text-white p-4 text-lg'>UTA Signup</h1>
+        <br />
+        <h2>Please signup to continue</h2>
+        <br />
+        <input
+          type="text"
+          placeholder="First Name"
+          value={firstname}
+          onChange={(e) => setFirstName(e.target.value)}
+          className="email_"
+          required
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          placeholder="Last Name"
+          value={lastname}
+          onChange={(e) => setLastName(e.target.value)}
+          className="email_"
+          required
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          placeholder="phone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          className="email_"
+          required
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="email_"
+          required
+        />
+        <br />
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="password_"
+          required
+        />
+        <br />
+        <br />
+        <input
+          type="password"
+          placeholder="confirm Password"
+          value={confirmPassword}
+          onChange={(e) => {setConfirmPassword(e.target.value);}}
+          className="password_"
+        />
+        {
+          !confirmMessage && <h1>Both passwords should match</h1>
+        }
+        <br />
+        <br />
+        <button onClick={handleSignup} className="login_button">
+          Signup
+        </button>
+        <br />
+        <br />
+        <button onClick={handleCancelButton} className="signup_button">
+          Cancel
+        </button>
+      </form>
     </div>
   ) : null;
 }
